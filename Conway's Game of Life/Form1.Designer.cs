608507507e02a9cc78cbcde442d6a8ffaed2af2f
Kotbenek <dominik.piatkowski@protonmail.com>
2020-11-19 15:30:06 +0100

    Initial commit

---
+++ Conway's Game of Life/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace Conway_s_Game_of_Life
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loadFromImageToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.simulationToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.startToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.stopToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.settingsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.picGrid = new System.Windows.Forms.PictureBox();
+            this.tmrStep = new System.Windows.Forms.Timer(this.components);
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.simulationToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1008, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.loadFromImageToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // loadFromImageToolStripMenuItem
+            // 
+            this.loadFromImageToolStripMenuItem.Name = "loadFromImageToolStripMenuItem";
+            this.loadFromImageToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.loadFromImageToolStripMenuItem.Text = "Load from image";
+            this.loadFromImageToolStripMenuItem.Click += new System.EventHandler(this.loadFromImageToolStripMenuItem_Click);
+            // 
+            // simulationToolStripMenuItem
+            // 
+            this.simulationToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.startToolStripMenuItem,
+            this.stopToolStripMenuItem,
+            this.settingsToolStripMenuItem});
+            this.simulationToolStripMenuItem.Name = "simulationToolStripMenuItem";
+            this.simulationToolStripMenuItem.Size = new System.Drawing.Size(76, 20);
+            this.simulationToolStripMenuItem.Text = "Simulation";
+            // 
+            // startToolStripMenuItem
+            // 
+            this.startToolStripMenuItem.Enabled = false;
+            this.startToolStripMenuItem.Name = "startToolStripMenuItem";
+            this.startToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.startToolStripMenuItem.Text = "Start";
+            this.startToolStripMenuItem.Click += new System.EventHandler(this.startToolStripMenuItem_Click);
+            // 
+            // stopToolStripMenuItem
+            // 
+            this.stopToolStripMenuItem.Enabled = false;
+            this.stopToolStripMenuItem.Name = "stopToolStripMenuItem";
+            this.stopToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.stopToolStripMenuItem.Text = "Stop";
+            this.stopToolStripMenuItem.Click += new System.EventHandler(this.stopToolStripMenuItem_Click);
+            // 
+            // settingsToolStripMenuItem
+            // 
+            this.settingsToolStripMenuItem.Name = "settingsToolStripMenuItem";
+            this.settingsToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.settingsToolStripMenuItem.Text = "Settings";
+            this.settingsToolStripMenuItem.Click += new System.EventHandler(this.settingsToolStripMenuItem_Click);
+            // 
+            // picGrid
+            // 
+            this.picGrid.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.picGrid.Location = new System.Drawing.Point(0, 24);
+            this.picGrid.Name = "picGrid";
+            this.picGrid.Size = new System.Drawing.Size(1008, 577);
+            this.picGrid.TabIndex = 1;
+            this.picGrid.TabStop = false;
+            // 
+            // tmrStep
+            // 
+            this.tmrStep.Interval = 50;
+            this.tmrStep.Tick += new System.EventHandler(this.tmrStep_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1008, 601);
+            this.Controls.Add(this.picGrid);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Conway\'s Game of Life";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picGrid)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem simulationToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem startToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem stopToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem settingsToolStripMenuItem;
+        private System.Windows.Forms.PictureBox picGrid;
+        private System.Windows.Forms.Timer tmrStep;
+        private System.Windows.Forms.ToolStripMenuItem loadFromImageToolStripMenuItem;
+    }
+}
+
---
+++ Conway's Game of Life/Form1.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace Conway_s_Game_of_Life
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //Global variables
+        byte[][] map_0;
+        byte[][] map_1;
+        int map_index;
+        int x = 0;
+        int y = 0;
+        Bitmap bitmap;
+        Bitmap bitmap_resized;
+        Rectangle b_rectangle;
+
+        /// <summary>
+        /// Function calculating next grid state
+        /// </summary>
+        /// <param name="array_input">Byte array containing current grid state</param>
+        /// <param name="array_output">Byte array that will contain the next grid state</param>
+        /// <param name="x">Width of the array</param>
+        /// <param name="y">Height of the array</param>
+        void calculate(byte[][] array_input, byte[][] array_output, int x, int y)
+        {
+            Parallel.For(1, x - 1, i =>
+            {
+                for (int j = 1; j < y - 1; j++)
+                {
+                    int neighbours = 0;
+
+                    for (int k = -1; k < 2; k++)
+                    {
+                        for (int l = -1; l < 2; l++)
+                        {
+                            if (k == 0 && l == 0) continue;
+
+                            if (array_input[i + k][j + l] == 1) neighbours++;
+                        }
+                    }
+
+                    if (neighbours == 3) array_output[i][j] = 1;
+                    else if (array_input[i][j] == 1 && (neighbours == 2 || neighbours == 3)) array_output[i][j] = 1;
+                    else array_output[i][j] = 0;
+                }
+            });
+
+            //Hidden borders cleanup
+            Parallel.For(0, x, i => array_output[i][0] = 0);
+            Parallel.For(0, x, i => array_output[i][y - 1] = 0);
+            Parallel.For(0, y, i => array_output[0][i] = 0);
+            Parallel.For(0, y, i => array_output[x - 1][i] = 0);
+        }
+
+        /// <summary>
+        /// Function that creates a Bitmap object, fills it with map data and displays it on screen using PictureBox control
+        /// </summary>
+        void draw()
+        {
+            //Fast drawing using unsafe code and parallel loops
+            unsafe
+            {
+                BitmapData data = bitmap.LockBits(b_rectangle, ImageLockMode.ReadWrite, bitmap.PixelFormat);
+
+                //Pointer to the beginning of bitmap data
+                byte* ptr = (byte*)data.Scan0;
+
+                //Using map_0
+                if (map_index == 0)
+                {
+                    Parallel.For(0, data.Height, y =>
+                    {
+                        byte* line = ptr + (y * data.Stride);
+
+                        for (int x = 0; x < data.Width; x++)
+                        {
+                            line[x] = map_0[x + 1][y + 1] == 1 ? (byte)0 : (byte)255;
+                        }
+                    });
+                }
+                //Using map_1
+                else
+                {
+                    Parallel.For(0, data.Height, y =>
+                    {
+                        byte* line = ptr + (y * data.Stride);
+
+                        for (int x = 0; x < data.Width; x++)
+                        {
+                            line[x] = map_1[x + 1][y + 1] == 1 ? (byte)0 : (byte)255;
+                        }
+                    });
+                }
+                bitmap.UnlockBits(data);
+            }
+            //Draw the resized bitmap
+            draw_resized();
+        }
+
+        /// <summary>
+        /// Function that calculates the biggest possible size of bitmap to fit inside the window, redraws map on the larger bitmap and displays it on screen
+        /// </summary>
+        void draw_resized()
+        {
+            //Calculate scale
+            int scale_x = picGrid.Width / bitmap.Width;
+            int scale_y = picGrid.Height / bitmap.Height;
+            int scale = scale_x < scale_y ? scale_x : scale_y;
+            //If the window is smaller than the bitmap, set scale to 1
+            if (scale == 0) scale = 1;
+
+            //Calculate new size
+            int width = bitmap.Width * scale;
+            int height = bitmap.Height * scale;
+
+            //If resize is needed, recreate bitmap_resized object with correct parameters
+            if (bitmap_resized.Width != width || bitmap_resized.Height != height)
+                bitmap_resized = new Bitmap(bitmap.Width * scale, bitmap.Height * scale, PixelFormat.Format24bppRgb);
+
+            //Redraw map on the larger bitmap
+            using (Graphics g = Graphics.FromImage(bitmap_resized))
+            {
+                g.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.NearestNeighbor;
+                g.PixelOffsetMode = System.Drawing.Drawing2D.PixelOffsetMode.Half;
+                g.DrawImage(bitmap, new Rectangle(0, 0, bitmap_resized.Width, bitmap_resized.Height));
+            }
+
+            //Display bitmap on the screen
+            picGrid.Image = bitmap_resized;
+        }
+
+        private void loadFromImageToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //Let user choose a file
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "PNG files (*.png)|*.png";
+
+            //If the file selection was successful
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    //Read the file
+                    Bitmap tmp = (Bitmap)Bitmap.FromFile(ofd.FileName);
+                    //Read the grid dimensions
+                    x = tmp.Width;
+                    y = tmp.Height;
+
+                    //Add one cell wide hidden border
+                    x += 2;
+                    y += 2;
+
+                    //Initialize byte arrays
+                    map_0 = new byte[x][];
+                    map_1 = new byte[x][];
+                    for (int i = 0; i < x; i++)
+                    {
+                        map_0[i] = new byte[y];
+                        map_1[i] = new byte[y];
+                    }
+
+                    //Iterate through pixels
+                    //If it's dark - it's a living cell
+                    //If it's not dark - it's a dead cell
+                    for (int i_x = 0; i_x < x - 2; i_x++)
+                    {
+                        for (int i_y = 0; i_y < y - 2; i_y++)
+                        {
+                            if (tmp.GetPixel(i_x, i_y).GetBrightness() < 0.1)
+                                map_0[i_x + 1][i_y + 1] = 1;
+                            else map_0[i_x + 1][i_y + 1] = 0;
+                        }
+                    }
+
+                    //Initializing global variables
+                    bitmap = new Bitmap(x - 2, y - 2, PixelFormat.Format8bppIndexed);
+                    bitmap_resized = new Bitmap(x - 2, y - 2);
+                    b_rectangle = new Rectangle(0, 0, x - 2, y - 2);
+                    map_index = 0;
+
+                    //Draw grid on the screen
+                    draw();
+                }
+                //Error handling
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message.ToString(), Application.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+
+                if (x > 0 && y > 0) startToolStripMenuItem.Enabled = true;
+            }
+        }
+
+        private void tmrStep_Tick(object sender, EventArgs e)
+        {
+            //Calculate next step using correct map
+            if (map_index == 0)
+            {
+                calculate(map_0, map_1, x, y);
+            }
+            else
+            {
+                calculate(map_1, map_0, x, y);
+            }
+            
+            //Switch correct map
+            map_index = map_index == 0 ? 1 : 0;
+
+            //Draw the calculated map
+            draw();
+        }
+
+        private void startToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            tmrStep.Start();
+            startToolStripMenuItem.Enabled = false;
+            stopToolStripMenuItem.Enabled = true;
+        }
+
+        private void stopToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            tmrStep.Stop();
+            startToolStripMenuItem.Enabled = true;
+            stopToolStripMenuItem.Enabled = false;
+        }
+
+        private void settingsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Not yet implemented!");
+        }
+    }
+}
